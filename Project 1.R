9c015500c6dfe9623acdd97536095b2efb0025ea
David Carlsson <57452866+davidtcarlsson@users.noreply.github.com>
2022-02-02 18:46:13 +0100

    Added binomal dist and a function to run the tests

--- Project 1.R
+++ Project 1.R
@@ -1,18 +1,73 @@
-counter <- 1
-for (n in c(10, 50, 100)) {
+# Sample estimates for the exp dist
+exp_dist <- function(xi, lambda, test_statistic) {
+  alpha <- 0.05
+  n <- length(xi)
+  lambda_hat <- n / sum(xi)
+  sd <- lambda_hat / sqrt(n)
+  test_statistic(lambda_hat, lambda, sd, alpha)
+}
+
+# Sample estimates for the binom dist
+bin_dist <- function(xi, p, test_statistic) {
+  alpha <- 0.05
+  n <- length(xi)
+  p_hat <- sum(xi) / n
+  sd <- sqrt(p_hat * (1 - p_hat) / n) 
+  test_statistic(p_hat, p, sd, alpha)
+}
+
+# Check if estimate fulfill the wald test
+wald_test <- function(theta_hat, theta, sd, alpha) {
+  abs(theta_hat - theta) / sd < qnorm(1 - alpha / 2)
+}
+
+# Check if estimate fulfill the score test
+score_test <- function(theta, sd, alpha) {
+  
+}
+
+# Generate 10000 samples and run the a given test on each of them
+# returns the proportion of tests where the confidence interval "covered"
+# the true parameter value
+run_exp_tests <- function(test_statistic) {
+  print("Exponential distribution")
+  # Loop trough a number of different lambdas
   for (lambda in c(0.1, 1, 10)) {
-    xi <- replicate(10000, rexp(n, lambda), simplify = FALSE)
-    test <- lapply(xi, KI_test, n, lambda)
-    mean(unlist(test))
+    # Loop through a number of different sample sizes
+    for (n in c(10, 50, 100)) {
+      # For a given lambda and sample size generate 10000 samples
+      xi <- replicate(10000, rexp(n, lambda), simplify = FALSE)
+      # Test each sample and return the proportion that fulfilled the test
+      results <- mean(unlist(lapply(X = xi, FUN = exp_dist, lambda, test_statistic)))
+      # Print out lambda, sample size and the proportion
+      print(paste("n =", n, "lambda =", lambda, "prop = ", results))
+    }
+    print("-----------------------------------------------")
   }
 }
 
-KI_test <- function(xi, n, lambda) {
-  lambda_hat <- n / sum(xi)
-  sd <- 1 / (sqrt(n) / lambda)
-  return((lambda_hat - qnorm(0.05/2, 0, 1) * sd < lambda) | (lambda_hat + qnorm(0.05/2, 0, 1) * sd > lambda)) 
+run_binom_tests <- function(test_statistic) {
+  print("Binomial distribution")
+  # Loop trough a number of different p's
+  for (p in c(0.1, 0.3, 0.5)) {
+    # Loop through a number of different sample sizes
+    for (n in c(10, 50, 100)) {
+      # For a given p and sample size generate 10000 samples
+      xi <- replicate(10000, rbinom(n = n, size = 1, prob = p), simplify = FALSE)
+      # Test each sample and return the proportion that fulfilled the test
+      results <- mean(unlist(lapply(X = xi, FUN = bin_dist, p, test_statistic)))
+      # Print out lambda, sample size and the proportion
+      print(paste("n =", n, "p =", p, "prop =", results))
+    }
+    print("-----------------------------------------------")
+  }
 }
 
-xi <- replicate(10000, rexp(200, 10), simplify = FALSE)
-test <- lapply(xi, KI_test, n=200, lambda=10)
-mean(unlist(test))
+run_exp_tests(wald_test)
+#run_exp_tests(score_test)
+
+run_binom_tests(wald_test)
+# run_binom_tests(score_test)
+
+
+
