67c9dc3bc99016940df5a9eb2abb1499dd320a1a
David Carlsson <57452866+davidtcarlsson@users.noreply.github.com>
2022-02-10 16:12:25 +0100

    Updated the functions on both distributions

--- Project 1/Project 1.R
+++ Project 1/Project 1.R
@@ -1,68 +1,43 @@
+library(magrittr)
+
 # Set a seed so that we are able to reproduce the results
 set.seed(2022)
 
 # The significance level we will use for both tests
 alpha <- 0.05
-
-# Calculates the MLE estimates the exponential distribution
-mle_exp <- function(xi) {
-  n <- length(xi)
-  lambda_hat <- n / sum(xi)
-  sd <- lambda_hat / sqrt(n)
-  return(c(lambda_hat, sd))
-}
-
-# Calculates the MLE estimates the binomial distribution
-mle_binom <- function(xi) {
-  n <- length(xi)
-  p_hat <- sum(xi) / n
-  sd <- sqrt(p_hat * (1 - p_hat) / n) 
-  return(c(p_hat, sd))
-}
-
-# Checks if KI covers the population parameter for our two tests
-wald_test <- function(xi, theta, mle_estimates) {
-  # Extract the parameters from the vector
-  theta_hat <- mle_estimates(xi)[1]
-  sd <- mle_estimates(xi)[2]
-  (abs(theta_hat - theta) / sd) > qnorm(1 - alpha / 2)
-}
-
-# Runs the score test for the exponential distribution
-score_test_exp <- function(xi, lambda0, mle_estimates) {
-  n <- length(xi)
-  xbar <- mean(xi)
-  sqrt(n) * abs(1 - lambda0 * xbar) > qnorm(1 - alpha / 2)
-}
-
-# Runs the score test for the binomial distribution
-score_test_binom <- function(xi, p0, mle_estimates) {
-  n <- length(xi)
-  xbar  <- mle_estimates(xi)[1]
-  (abs(xbar - p0) / sqrt((p0 * (1 - p0)) / n)) > qnorm(1 - alpha / 2)
-}
+# Number of simulated datasets 
+iterations <- 10000
 
 # Generate 10000 samples and run the a given test on each of them
 # returns the proportion of tests where the confidence interval "covered"
 # the true parameter value
 run_exp_tests <- function() {
   # Create a matrix that we will add our results to
-  exp_output <- matrix(nrow = 9, ncol = 5)
+  exp_output <- matrix(nrow = 9, ncol = 4)
   colnames(exp_output) <- c("n", "p", "Score-testet", 
-                            "Wald-testet", "Absolut diff")
+                            "Wald-testet")
   counter <- 1
   # Loop trough a number of different lambdas
   for (lambda in c(0.1, 1, 10)) {
     # Loop through a number of different sample sizes
     for (n in c(10, 50, 100)) {
       # For a given lambda and sample size generate 10000 samples
-      xi <- replicate(10000, rexp(n, lambda), simplify = FALSE)
-      # Test each sample and return the proportion that fulfilled the test
-      results_score <- mean(unlist(lapply(X = xi, FUN = score_test_exp, lambda, mle_exp)))
-      results_wald <- mean(unlist(lapply(X = xi, FUN = wald_test, lambda, mle_exp)))
+      xi <- replicate(iterations, rexp(n, lambda), simplify = FALSE)
+      # Run a function that applies the score test
+      result_score <- lapply(xi, FUN = function(xi) {
+        n <- length(xi)
+        xbar <- mean(xi)
+        sqrt(n) * abs(1 - lambda * xbar) > qnorm(1 - alpha / 2)
+      }) %>% unlist %>% mean
+      # Run a function that applies the wald test
+      result_wald <- lapply(xi, FUN = function(xi) {
+        lambda_hat <- n / sum(xi)
+        sd <- lambda_hat / sqrt(n)
+        (abs(lambda_hat - lambda) / sd) > qnorm(1 - alpha / 2)
+      }) %>% unlist %>% mean
       # Add the results to the table
-      exp_output[counter, ] <- c(n, lambda, results_score, 
-                                 results_wald, abs(results_score-results_wald))
+      exp_output[counter, ] <- c(n, lambda, result_score, 
+                                 result_wald)
       counter <- counter + 1
     }
   }
@@ -71,24 +46,30 @@ run_exp_tests <- function() {
 
 run_binom_tests <- function() {
   # Create a matrix that we will add our results to
-  binom_output <- matrix(nrow = 9, ncol = 5)
+  binom_output <- matrix(nrow = 9, ncol = 4)
   colnames(binom_output) <- c("n", "Lambda", "Score-testet", 
-                              "Wald-testet", "Absolut diff")
+                              "Wald-testet")
   counter <- 1
   # Loop trough a number of different p's
   for (p in c(0.1, 0.3, 0.5)) {
     # Loop through a number of different sample sizes
     for (n in c(10, 50, 100)) {
       # For a given p and sample size generate 10000 samples
-      xi <- replicate(10000, rbinom(n = n, size = 1, prob = p), simplify = FALSE)
-      # Test each sample and return the proportion that fulfilled the test
-      results_score <- mean(unlist(lapply(X = xi, 
-                                          FUN = score_test_binom, p, mle_binom)))
-      results_wald <- mean(unlist(lapply(X = xi, 
-                                         FUN = wald_test, p, mle_binom)))
+      xi <- replicate(iterations, rbinom(n = 1, size = n, prob = p), simplify = FALSE)
+      # Run a function that applies the score test
+      result_score <- lapply(xi, FUN = function(xi) { 
+        xbar <- xi / n 
+        (abs(xbar - p) / sqrt((p * (1 - p)) / n)) > qnorm(1 - alpha / 2)
+      }) %>% unlist %>% mean
+      # Run a function that applies the wald test
+      result_wald <- lapply(xi, FUN = function(xi) {
+        xbar <- xi / n
+        sd <- sqrt(xbar * (1 - xbar) / n) 
+        (abs(xbar - p) / sd) > qnorm(1 - alpha / 2)
+      }) %>% unlist %>% mean
       # Add the results to the table
-      binom_output[counter, ] <- c(n, p, results_score, 
-                                   results_wald, abs(results_score-results_wald))
+      binom_output[counter, ] <- c(n, p, result_score, 
+                                   result_wald)
       counter <- counter + 1
     }
   }
